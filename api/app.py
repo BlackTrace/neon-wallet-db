09c749243dd5c6d46676272e8f7c653bdb15f747
Erik van den Brink <git@erikvandenbrink.com>
2018-01-09 22:14:01 +0100

    remove old api

--- api/app.py
+++ api/app.py
@@ -1,8 +1,6 @@
 from .server import application
-from .api_old import api_old
 from .api import api
 
-application.register_blueprint(api_old)
 application.register_blueprint(api)
 
 if __name__ == "__main__":
